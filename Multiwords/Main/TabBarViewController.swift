93818ebd2f11630ed2bb34a322e54fee1d50d28b
Alina <alina.knyazevaa@gmail.com>
2017-10-18 19:55:11 +0300

    fixproject

---
+++ Multiwords/Main/TabBarViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TabBarViewController.swift
+//  Multiwords
+//
+//  Created by Алина Князева on 16.10.2017.
+//  Copyright © 2017 Алина Князева. All rights reserved.
+//
+
+import Foundation
